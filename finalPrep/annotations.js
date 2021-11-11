9995fc021cd7262e3a2b9e570f51d61f2400f71a
Emili Kaiman <emilikaiman@gmail.com>
2021-11-11 14:29:42 -0700

    Add final prep

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,41 +2,73 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+// Declare function called buildABear that takes 5 arguements
 function buildABear(name, age, fur, clothes, specialPower) {
+  //Declare variable named greeting that is assigned to the value of a string
+  //that is interpolated with a variable
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  // Declare variable named demographics that is assigned to an array
   var demographics = [name, age];
+  // Declare a variable named powerSaying that is assigned to the value of a string
+  // that uses concatenation with a variable
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+  // Declare a variable called builtBear that is assigned to the value of an object
   var builtBear = {
+    // Declare key-value pair, basicInfo is the key
     basicInfo: demographics,
+    // Declare key-value pair, clothes is the key
     clothes: clothes,
+    // Declare key-value pair, exterior is the key
     exterior: fur,
+    // Declare key-value pair, cost is the key
     cost: 49.99,
+    // Declare key-value pair, sayings is the key and the value is an array
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
+    // Declare key-value pair, idCuddly is the key and the value is a boolean
     isCuddly: true,
   };
-
+  // Return value will hold the builtBear object information
   return builtBear
 }
 
+//Pass arguements through buildABear function, holding an array for clothes arguement
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
+//Pass arguements through buildABear function, holding an array for clothes arguement
+//(name, age, fur, clothes, specialPower)
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
 
 //FizzBuzz
-function fizzBuzz(num1, num2, range) {
+//Declare a function that take 3 arguements
+function fizzBuzz(num1, num2, range)
+// Create a loop starting at 0. It can run less than or equal to the declared range, and will
+//add one to i each time it runs until it can not run
   for (var i = 0; i <= range; i++) {
+    // if the remainder of i divided bu num1 is strictly equal to 0, AND if the remainer of
+    // i divided by num2 is strictly zero, both evaluate to true then
     if (i % num1 === 0 && i % num2 === 0) {
+      //log to the console fizzbuzz
       console.log('fizzbuzz');
+      //otherwise if the remainder of i divided by num1 is strictly equal to 0
     } else if (i % num1 === 0) {
+      //log to the console fizz
       console.log('fizz');
+      // otherwise if the remainer of i divided by num2 is strictly zero
     } else if (i % num2 === 0) {
+      // log to the console buzz
       console.log('buzz');
+      // if none of the above evaluates to true
     } else {
+      //log to the console i
       console.log(i);
     }
   }
 }
 
+//pass arguement through fizzBuzz arguement up to 100 times with num1 being 3 and num2 being 5
+// or until function returns false
 fizzBuzz(3, 5, 100);
+//pass arguement through fizzBuzz arguement up to 400 times with num1 being 5 and num2 being 8
+// or until function returns false
 fizzbuzz(5, 8, 400);
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,40 +1,66 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
+var heroName = 'Superman';
+var specialAbility = 'Flying'
 
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
+var greeting = 'Hello world! I am ' + heroName;
+var catchphrase = `Truth, justice, and the American way with ${specialAbility}`;
 
 // Declare two variables - power AND energy - set to integers
-
+var power = 99;
+var energy = 100;
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
+var fullPower = power * 500;
+var fullEnergy = energy + 150;
 
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
+var isHuman = true;
+var identityConcealed = false;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
-
+var archEnemies = ['Lex Luthor', 'Ursa', 'Non'];
+var sidekicks = ['Lois Lane', 'cheese', 'wine'];
 // Print the first sidekick to your console
+console.log(sidekicks[0]);
 
 // Print the last archEnemy to the console
-
+console.log(archEnemies[2])
+;
 // Write some code to add a new archEnemy to the archEnemies array
-
+archEnemies.push('Green Lantern');
 // Print the archEnemies array to console to ensure you added a new archEnemy
-
+console.log(archEnemies);
 // Remove the first sidekick from the sidekicks array
+sidekicks.shift();
+console.log(sidekicks);
 
 // Print the sidekicks array to console to ensure you added a new sidekick
+sidekicks.unshift('crackers');
+console.log(sidekicks);
 
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+  if (dangerLevel > 50) {
+      console.log(badExcuse);
+    } else if (dangerLevel >= 10) {
+      console.log(saveTheDay);
+    } else {
+      console.log('Meh. Hard pass.');
+    }
+};
+
 
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
@@ -44,9 +70,9 @@
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!';
 var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.';
-// assessSituation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-//assessSituation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
-//assessSituation(3, announcement, excuse) > should print - "Meh. Hard pass."
+assessSituation(99, announcement, excuse) //Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
+assessSituation(21, announcement, excuse) //> should print - 'Never fear, the Courageous Curly Bracket is here!'
+assessSituation(3, announcement, excuse) //> should print - "Meh. Hard pass."
 
 // Declare a new variable - scaryMonster - assigned to an Object with the following key/values
 //   - name (string)
@@ -55,30 +81,71 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - citiesDestroyed (array)
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
-
+var scaryMonster = {
+  name: 'Giant Bug',
+  smell: 'stinky',
+  weight: 389,
+  citiesDestroyed: ['Omaha', 'Denver', 'New York City'],
+  luckyNumbers: [3, 7, 28],
+  address: {
+    number: 30,
+    street: 'Fox',
+    state: 'Colorado',
+    zip: '80110'
+  }
+};
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
+
+class SuperHero {
+  constructor(name, superpower, age) {
+    this.name = name;
+    this.superpower = superpower;
+    this.age = age;
+    this.archNemesis = 'The Syntax Error';
+    this.powerLevel = 100;
+    this.energyLevel = 50
+  }
+  sayName() {
+    console.log(this.name);
+  }
+  maximizeEnergy() {
+    this.energyLevel = 1000;
+  }
+  gainPower(num){
+    this.powerLevel = this.powerLevel + num
+  }
+}
+
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
 //   - maximizeEnergy, should update the energyLevel to 1000
 //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
-
 // - Create 2 instances of your SuperHero class
+var batman = new SuperHero('Batman', 'Flying', 89);
+var wonderWoman = new SuperHero('WonderWoman', 'strength', 45);
 
+console.log(batman);
+batman.gainPower(30);
+console.log(batman);
+wonderWoman.sayName();
 
 // Reflection
 // What parts were most difficult about this exerise?
+//classes and class methods were most difficult to me
 
 // What parts felt most comfortable to you?
+//creating variables and arrays
+
 
 // What skills do you need to continue to practice before starting Mod 1?
-
+//calling classes and methods and if statements within a function
